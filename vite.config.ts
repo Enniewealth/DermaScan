0d4ef71bb9f1d4bafc2100f44b25017a7c099371
uniquenny <eniolamiseyi62@gmail.com>
2026-06-15 15:04:27 +0100

    Add .npmrc and restore legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ['iOS >= 13', 'Safari >= 13', 'last 2 versions'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
   base: '/',
   build: {
     target: ['es2015', 'safari13'],
